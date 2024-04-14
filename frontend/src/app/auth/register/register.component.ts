eeca5f25c14d414a608a265b03bc5dde3baf0c62
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2024-04-14 17:37:32 +0200

    register template frontend

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, ValidationErrors, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {ToastController} from "@ionic/angular";
 
 @Component({
   selector: 'app-register',
@@ -7,8 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent  implements OnInit {
 
-  constructor() { }
+  readonly form = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    repeatPassword: ['', [Validators.required]],
+    phone: ['', Validators.required],
+  });
 
-  ngOnInit() {}
+  isPasswordSame = false;
+  constructor(
+    private readonly fb: FormBuilder,
+  ) {
+  }
+
+  ngOnInit() {
+    // Subscribe to changes in password and repeatPassword fields
+    this.form.get('password')!.valueChanges.subscribe(() => this.checkPasswords());
+    this.form.get('repeatPassword')!.valueChanges.subscribe(() => this.checkPasswords());
+  }
+
+  get name() {
+    return this.form.controls.name;
+  }
+
+  get email() {
+    return this.form.controls.email;
+  }
+
+  get phone() {
+    return this.form.controls.phone;
+  }
+
+  get password() {
+    return this.form.controls.password;
+  }
+
+  get repeatPassword() {
+    return this.form.controls.repeatPassword;
+  }
+
+  register() {
+
+  }
+
+
+  checkPasswords() {
+    const password = this.form.get('password')!.value;
+    const repeatPassword = this.form.get('repeatPassword')!.value;
+    this.isPasswordSame = password === repeatPassword;
+  }
 
 }
