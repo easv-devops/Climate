b71dc7d8f2e9845a3544673d39ad6e117ef8c734
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-04-16 11:07:11 +0200

    Update V2__PasswordHash.sql

--- db/sql/V2__PasswordHash.sql
+++ db/sql/V2__PasswordHash.sql
@@ -2,7 +2,7 @@
 CREATE TABLE PasswordHash (
     UserId                  INT PRIMARY KEY,
     Hash                    TEXT NOT NULL,
-    Salt                    VARCHAR(100) NOT NULL,
+    Salt                    VARCHAR(255) NOT NULL,
     Algorithm               VARCHAR(50) NOT NULL,
     FOREIGN KEY (UserId) REFERENCES User(Id)
 );
