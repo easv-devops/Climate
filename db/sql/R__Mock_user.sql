196c5867f51e45aa3eed49b8b6f2157149adbf1c
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-04-16 10:35:02 +0200

    Create R__Mock_user.sql

---
+++ db/sql/R__Mock_user.sql
@@ -0,0 +1,19 @@
+﻿-- Insert user data
+INSERT INTO User (Email)
+VALUES ('kfe@gmail.com');
+
+-- Insert user information
+INSERT INTO UserInformation (UserId, FirstName, LastName)
+VALUES (1, 'John', 'Doe');
+
+-- Insert contact information
+INSERT INTO ContactInformation (UserId, CountryCode, Number)
+VALUES (1, '+1', '1234567890');
+
+-- Insert user status
+INSERT INTO UserStatus (UserId, BanTimestamp)
+VALUES (1, NULL);
+
+-- Insert password hash information
+INSERT INTO PasswordHash (UserId, Hash, Salt, Algorithm)
+VALUES (1, '1EJybmIbon7kimzpBZXA17OxI3/iVLZK8euSAloQgK3W8ibEJ8G/Ql2J4kjtDDMRV5sN71LEgRuL+lXyP9dOHz9IuMXuWjTdFSwkKaDNbiNa9MsWy/dngKWo04jYvG8Tb26UV0Bnxd83V9zQZCPdPSQXENoRvPOhnDZKaayFYuRz4pVkBrooL9Hu9EgrCzE9Z3kExf+w1BwR/hqVip2wj+W3mxBwTWgm5hhsko1TZqr3d+HWPAeaFmaNTmwuG0miPhA8H9C4/V0mUs62V2zJkZEVP3QEipvTvkCyctxq7U89NSLwVIGiEsmFG/sZ1EqXnXpmpbV1PQ7pkDYFad+pzQ==', 'sMQAck67hWo2asVpqlbmmGVFj3jo6i86oZVTQh3c3wOpKd0LO8oxqSYhveceXkLrXlCKIIVFB+IRPXrcE3ZkFdVKmG5A7gOyvWwkOltwOytSDoPHmT3+aWUS0sFjO89RMbJxCncsghBbtF3a9hHtr/7/NcexUj8wJQz48gq6izw=', 'argon2id');
