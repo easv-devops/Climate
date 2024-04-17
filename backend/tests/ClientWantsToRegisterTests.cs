500ea69f2e95f9266350ee6f966e736709a596d9
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2024-04-17 22:26:09 +0200

    test register

---
+++ backend/tests/ClientWantsToRegisterTests.cs
@@ -0,0 +1,54 @@
+﻿using api.clientEventHandlers;
+using api.serverEventModels;
+using infrastructure.Models.serverEvents;
+using tests.WebSocket;
+
+namespace tests;
+
+public class ClientWantsToRegisterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Startup.Start(null);
+    }
+    
+    [TestCase("user@example.com", "12345678", "register", "+1234567890", "John", "Doe", "+45", TestName = "Valid")]
+    [TestCase("user@example.com", "87654321", "register", "+1234567890", "John", "Doe", "+44", TestName = "Invalid password")]
+    [TestCase("userATexample.com", "12345678", "register", "+1234567890", "John", "Doe", "+80", TestName = "invalid email")]
+    public async Task RegisterTest(string email, string password, string eventType, string phone, string firstName, string lastName, string countryCode)
+    {
+        var ws = await new WebSocketTestClient().ConnectAsync();
+
+        await ws.DoAndAssert(new ClientWantsToRegisterDto
+        {
+            Email = email,
+            Password = password,
+            Phone = phone,
+            FirstName = firstName,
+            LastName = lastName,
+            CountryCode = countryCode
+        }, fromServer =>
+        {
+            return fromServer.Count(dto =>
+            {
+                Console.WriteLine("Event type: " + dto.eventType + ". Count: " + fromServer.Count);
+                string testName = TestContext.CurrentContext.Test.Name;
+                switch (testName)
+                {
+                    case "Valid":
+                        return dto.eventType == nameof(ServerAuthenticatesUser);
+                    case "Invalid password":
+                        return dto.eventType == nameof(ServerSendsErrorMessageToClient);
+                    case "Invalid email":
+                        return dto.eventType == nameof(ServerSendsErrorMessageToClient);
+                    default:
+                        return false;
+                }
+            }) == 1;
+        });
+    }
+    
+    
+    
+}
