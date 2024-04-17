33adb58fe6f19cd27c850d03f2ced86c5e1bb2ed
steffaneckeroth <114472157+steffaneckeroth@users.noreply.github.com>
2024-04-17 13:03:20 +0200

    resetpassword component
    Created resetpassword component in frontend. The HTML is decent, no css (yet?). Prepared for a submit/resetpassword metode, its empty for now tho. In the backend I created a ClientWantsToResetPassword.cs file. Right now its pretty much empty. So, this commit only contains preparation type shii :)

---
+++ backend/api/clientEventHandlers/ClientWantsToResetPassword.cs
@@ -0,0 +1,22 @@
+﻿using Fleck;
+using lib;
+
+namespace api.clientEventHandlers;
+
+
+public class ClientWantsToResetPasswordDto : BaseDto
+{
+    public string Email { get; set; }
+}
+
+
+
+
+
+public class ClientWantsToResetPassword : BaseEventHandler<ClientWantsToResetPasswordDto>
+{
+    public override Task Handle(ClientWantsToResetPasswordDto dto, IWebSocketConnection socket)
+    {
+        throw new NotImplementedException();
+    }
+}
--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import {BrowserModule} from "@angular/platform-browser";
@@ -9,15 +9,17 @@ import {AuthComponent} from "./auth.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {ResetpasswordComponent} from "./resetpassword/resetpassword.component";
 
 
 @NgModule({
-  declarations: [AuthComponent, LoginComponent, RegisterComponent],
+  declarations: [AuthComponent, LoginComponent, RegisterComponent, ResetpasswordComponent],
   imports: [
     CommonModule,
     IonicModule,
     AuthRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgOptimizedImage
   ]
 })
 export class AuthModule { }
