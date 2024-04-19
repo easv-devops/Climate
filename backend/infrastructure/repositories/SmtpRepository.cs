8a1e922ae1f19c2fa0322ac3b3f00c6aa4c0ebc6
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-04-19 12:52:17 +0200

    Removed unused method

--- backend/infrastructure/repositories/SmtpRepository.cs
+++ backend/infrastructure/repositories/SmtpRepository.cs
@@ -7,31 +7,6 @@ namespace infrastructure;
 
 public class SmtpRepository
 {
-    public bool SendEmail1(string body)
-    {
-        using (var client = new SmtpClient())
-        {
-            client.Host = "smtp.gmail.com";
-            client.Port = 587;
-            client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.UseDefaultCredentials = false;
-            client.EnableSsl = true;
-            client.Credentials = new NetworkCredential("climatenoti@GMAIL.COM", Environment.GetEnvironmentVariable(EnvVarKeys.MailPassword.ToString()));
-            using (var message = new MailMessage(
-                       from: new MailAddress("climatenoti@GMAIL.COM", "Your Climate"),
-                       to: new MailAddress("kennidamkier@gmail.com", "kenni")
-                   ))
-            {
-
-                message.Subject = "Reset Password";
-                message.Body = body;
-
-                client.Send(message);
-            }
-        }
-        return true;
-    }
-    
     public bool SendEmail(EmailDto emailContent)
     {
         using (var client = new SmtpClient())
