922745ecfe8de5d591e4d2f983a1c3dffbfce8a9
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2024-04-26 12:58:12 +0200

    Update ParticlesRepository.cs

--- backend/infrastructure/repositories/readingsRepositories/ParticlesRepository.cs
+++ backend/infrastructure/repositories/readingsRepositories/ParticlesRepository.cs
@@ -1,4 +1,7 @@
-﻿using infrastructure.Models;
+﻿using System.Data.SqlTypes;
+using Dapper;
+using infrastructure.Models;
+using MySqlConnector;
 
 namespace infrastructure.repositories.readingsRepositories;
 
@@ -13,12 +16,55 @@ public class ParticlesRepository
     
     public bool SaveParticle25List(int deviceId, List<SensorDto> particles25List)
     {
-     
-        throw new NotImplementedException("temperature repository, not implemented");
+        using var connection = new MySqlConnection(_connectionString);
+        try
+        {
+            connection.Open();
+            foreach (var particle in particles25List)
+            {
+                var sql = @"INSERT INTO ReadingParticle2_5 (DeviceId, Timestamp, P2_5) 
+                        VALUES (@DeviceId, @Timestamp, @P2_5)";
+                connection.Execute(sql, new
+                {
+                    DeviceId = deviceId,
+                    Timestamp = particle.TimeStamp,
+                    P2_5 = particle.Value
+                });
+            }
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw new SqlTypeException("Failed to save Particle25 readings", ex);
+        }
     }
+
     
     public bool SaveParticle100List(int deviceId, List<SensorDto> particles100List)
     {
-        throw new NotImplementedException("temperature repository, not implemented");
+        using var connection = new MySqlConnection(_connectionString);
+        try
+        {
+            connection.Open();
+            foreach (var particle in particles100List)
+            {
+                var sql = @"INSERT INTO ReadingParticle10 (DeviceId, Timestamp, P10) 
+                        VALUES (@DeviceId, @Timestamp, @P10)";
+                connection.Execute(sql, new
+                {
+                    DeviceId = deviceId,
+                    Timestamp = particle.TimeStamp,
+                    P10 = particle.Value
+                });
+            }
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw new SqlTypeException("Failed to save Particle100 readings", ex);
+        }
     }
+
 }
