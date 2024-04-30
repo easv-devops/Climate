88a835e72ca82b8d9ad444927340745279a7ef08
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-04-30 18:34:39 +0200

    frontend>baseDto eksplicit eventType test

--- frontend/src/models/baseDto.ts
+++ frontend/src/models/baseDto.ts
@@ -1,8 +1,8 @@
 export class BaseDto<T> {
   eventType: string;
 
-  constructor(init?: Partial<T>) {
-    this.eventType = this.constructor.name;
+  constructor(eventType: string, init?: Partial<T>) {
+    this.eventType = eventType;
     Object.assign(this, init);
   }
 }
