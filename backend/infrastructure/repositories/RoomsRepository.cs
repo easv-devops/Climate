ad4a5a33e70033dad62c8152143ed14641da8f81
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2024-05-07 12:01:03 +0200

    delete repo

--- backend/infrastructure/repositories/RoomsRepository.cs
+++ backend/infrastructure/repositories/RoomsRepository.cs
@@ -50,14 +50,10 @@ public class RoomsRepository
         }
     }
 
-    
-    
-    
-    
-    
-    
-    //todo should delete all devices in the room maybe? or what are the status..
+
     /**
+     * 
+     */
     public bool DeleteRoom(Room room)
     {
         using var connection = new MySqlConnection(_connectionString);
@@ -65,65 +61,19 @@ public class RoomsRepository
         {
             connection.Open();
             string query = @"
-                DELETE FROM climate.Room WHERE Id = @id AND UserId = @UserId;
+                DELETE FROM climate.Room WHERE Id = @id;
                 ";
-            connection.Execute(query, new { id = room.Id, UserId = room.UserId });
-            return true;
+            return connection.Execute(query, new { Id = room.Id }) == 1;
         }
         catch (Exception e)
         {
             throw new SqlTypeException("Failed to delete the specific room", e);
         }
     }
-    */
 
     
-
-
-
-    /**
-    public Room EditRoom(Room room)
-    {
-        using var connection = new MySqlConnection(_connectionString);
-        try
-        {
-            connection.Open();
-            string query = @"
-                UPDATE climate.Room 
-                SET RoomName = @roomName 
-                WHERE Id = @id 
-                AND UserId = @UserId;
-            ";
-            Room rooms = connection.ExecuteScalar<Room>(query,
-                new { roomName = room.RoomName, id = room.Id, UserId = room.UserId });
-
-            return rooms;
-        }
-        catch (Exception e)
-        {
-            throw new SqlTypeException("Failed to update the given room", e);
-        }
-    }
-
-    public Room getRoomById(Room room)
-    {
-        using var connection = new MySqlConnection(_connectionString);
-        try
-        {
-            connection.Open();
-            string query = @"
-                SELECT * FROM climate.Room WHERE Id = @id AND UserId = @UserId
-                ";
-            
-            Room specificRoom = connection.QueryFirstOrDefault<Room>(query, new { id = room.Id, UserId = room.UserId });
-            return specificRoom;
-        }
-        catch (Exception e)
-        {
-            throw new SqlTypeException("Failed to get the specific room", e);
-        }
-    }
-    */
+    
+ 
     public RoomWithId EditRoom(Room dto)
     {
         using var connection = new MySqlConnection(_connectionString);
