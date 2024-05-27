e86327c47244ece073729b43aab3aa10cade7242
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-05-27 22:17:17 +0200

    ClientWantsToGetDeviceIdsForRoomTest

---
+++ backend/tests/ClientWantsToGetDeviceIdsForRoomTest.cs
@@ -0,0 +1,38 @@
+﻿using api;
+using api.clientEventHandlers;
+using api.helpers;
+using tests.WebSocket;
+
+namespace tests;
+
+public class ClientWantsToGetDeviceIdsForRoomTest
+{
+    [SetUp]
+    public void Setup()
+    {
+        FlywayDbTestRebuilder.ExecuteMigrations();
+        Startup.Start(null, Environment.GetEnvironmentVariable(EnvVarKeys.dbtestconn.ToString()));
+    }
+    
+    [Test]
+    public async Task ClientWantsToGetDeviceIdsForRoom()
+    {
+        var ws = await new WebSocketTestClient().ConnectAsync();
+
+        // Arrange: Log in
+        ws.Send(new ClientWantsToSignInDto
+        {
+            email = "user@example.com",
+            password = "12345678"
+        });
+        
+        // Act: Get device ids for room 1
+        await ws.DoAndAssert(new ClientWantsToGetDeviceIdsForRoomDto()
+        {
+            RoomId = 1
+        }, fromServer =>
+        {
+            return fromServer.Count(dto => dto.eventType == nameof(ServerSendsDeviceIdListForRoom)) == 1; 
+        });
+    }
+}
