959dd0fd902ead2d9bfaa0feec855c48c5b18d16
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-05-30 11:06:55 +0200

    Update AuthGuard.ts

--- frontend/src/app/guards/AuthGuard.ts
+++ frontend/src/app/guards/AuthGuard.ts
@@ -1,16 +1,12 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import {WebSocketConnectionService} from "../web-socket-connection.service";
-import {ToastController} from "@ionic/angular";
+import {Injectable} from '@angular/core';
+import {CanActivate, Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor( private router: Router,
-               private ws: WebSocketConnectionService,
-               private toast: ToastController) {}
+  constructor( private router: Router) {}
 
   async canActivate() {
     // Tjek om brugeren har en gyldig JWT
@@ -18,11 +14,6 @@ export class AuthGuard implements CanActivate {
       return true; // Tillad adgang til ruten, hvis brugeren er logget ind
 
     } else {
-      await (await this.toast.create({
-        message: "Please login to access the requested URL",
-        color: "warning",
-        duration: 5000
-      })).present();
 
       // Hvis brugeren ikke er logget ind, omdiriger til login-siden
       this.router.navigate(['/auth/login']);
@@ -31,8 +22,7 @@ export class AuthGuard implements CanActivate {
   }
 
   isLoggedIn(): boolean {
-    const currentJwt = this.ws.jwtSubject.getValue();
+    const currentJwt = localStorage.getItem('jwt');
     return currentJwt !== undefined && currentJwt !== '';
-
   }
 }
